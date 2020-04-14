94ec2895dbc51d42a1318dcfc376ddee7e47a33f
pachkovska <pachkovska@gmail.com>
2020-04-13 22:54:04 -0700

    Updates

---
+++ precache-manifest.8543ee2bc0f265d03ca8052c1b25b9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34caa93239b2c19b5c16fa6d3f4db4a7",
+    "url": "/yet-another-weather-app/index.html"
+  },
+  {
+    "revision": "bc2c07a59ba02dc33877",
+    "url": "/yet-another-weather-app/static/css/main.8343e132.chunk.css"
+  },
+  {
+    "revision": "5155a0ade0a14028e3dd",
+    "url": "/yet-another-weather-app/static/js/2.2aef1af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yet-another-weather-app/static/js/2.2aef1af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2c07a59ba02dc33877",
+    "url": "/yet-another-weather-app/static/js/main.bf182cc5.chunk.js"
+  },
+  {
+    "revision": "40061a8cbbdaa2a9c584",
+    "url": "/yet-another-weather-app/static/js/runtime-main.7756bef8.js"
+  }
+]);
